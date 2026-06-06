519bd6236000ed1b0af2de7c963b72ecdf712267
y0b00n1 <fabianbaezmora@gmail.com>
2026-06-06 15:45:53 -0600

    Actualización logo y estilos CRTV

--- vistas/vista_superior.php
+++ vistas/vista_superior.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./css/main.css">
-        <title>CRTV</title>
-    </head>
-    <body>
-            <header>
-                <menu>
-                    <div class="menu-izquierda">
-                        <img src="./Logos/logo.png" alt="CRTV">
-                    </div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>CRTV</title>
+</head>
+<body>
 
-                    <div class="menu-derecha">
-                        <a href="index.php">Inicio</a>
-                        <a href="IPTV.php">IPTV</a>
-                        <a href="FLUJO_TV.php">Flujo_TV</a>
-                        <a href="CONTACTO.php">Contacto</a>
-                    </div>
-                 </menu>
-            </header>
+    <header>
+        <menu>
+            <div class="menu-izquierda">
+                <img src="./Logos/logo.png" alt="CRTV">
+            </div>
+
+            <div class="menu-derecha">
+                <a href="index.php">Inicio</a>
+                <a href="IPTV.php">IPTV</a>
+                <a href="FLUJO_TV.php">Flujo TV</a>
+                <a href="CONTACTO.php">Contacto</a>
+            </div>
+        </menu>
+    </header>
+
+</body>
+</html>
